de2d2337e72e0f4729327544c967802607c26822
automatic9045 <67314487+automatic9045@users.noreply.github.com>
2025-03-14 19:18:51 +0900

    chore: バージョンを2.0.50314.1に

--- Automatic9045.BveEx.RichLoad/Properties/AssemblyInfo.cs
+++ Automatic9045.BveEx.RichLoad/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("リッチなシナリオ読込画面を実装できるようにします。")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("automatic9045")]
-[assembly: AssemblyProduct("AtsEX プラグイン")]
+[assembly: AssemblyProduct("BveEX プラグイン")]
 [assembly: AssemblyCopyright("Copyright © 2024 automatic9045")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.41222.1")]
+[assembly: AssemblyVersion("2.0.50314.1")]
